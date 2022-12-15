4653e4976ccdf02743dc7efecda1ba1d0770ca5d
4ndrei <andreygames5@mail.ru>
2022-12-15 13:14:43 +0700

    dotenv fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.GOERLI_PRIVATE_KEY],
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_API_KEY}`,
+      accounts: [process.env.REACT_APP_GOERLI_PRIVATE_KEY],
     },
   },
 };
